537a4600acebf8980302303450d75e086e610b17
Johnny Kim <spyderkim@gmail.com>
2019-02-13 18:29:31 -0500

    created forum, workspace, settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
 
-
+  resources :forum
+  resources :settings
+  resources :workspace
 end
