97f0bd42a7126fa1c1657e920a50f5c6a74e98ca
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-02-14 17:18:25 -0500

    Created new admin account

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_175050) do
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
