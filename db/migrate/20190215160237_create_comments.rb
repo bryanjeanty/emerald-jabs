0385df0957363beec2df7e44129a9e75c888aa9b
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-02-15 14:48:38 -0500

    Updated comments table

--- db/migrate/20190215160237_create_comments.rb
+++ db/migrate/20190215160237_create_comments.rb
@@ -3,6 +3,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :title
       t.text :content
+      t.references :post
+      t.references :user
       t.timestamps
     end
   end
