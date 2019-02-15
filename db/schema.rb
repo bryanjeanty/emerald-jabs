f33e7e417c8a532bda01c7e6b2ea64407d0a5624
Bola Muyis <bolamuyis@gmail.com>
2019-02-15 10:32:13 -0500

    changes to registrations: edit and new

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_162206) do
-
+ActiveRecord::Schema.define(version: 2019_02_14_225939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
