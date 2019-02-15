5b92714767f59a6f33778a52b66efcf59fdf6ea2
Bola Muyis <bolamuyis@gmail.com>
2019-02-15 14:26:34 -0500

    more nav progress after lunch push

--- db/schema.rb
+++ db/schema.rb
@@ -64,11 +64,10 @@ ActiveRecord::Schema.define(version: 2019_02_15_160237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
