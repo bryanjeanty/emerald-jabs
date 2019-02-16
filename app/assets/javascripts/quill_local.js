16af051d02bb61fa18d0e3526c8b6515deb98662
Johnny Kim <spyderkim@gmail.com>
2019-02-16 18:31:48 -0500

    finals

---
+++ app/assets/javascripts/quill_local.js
@@ -0,0 +1,13 @@
+
+var form = document.querySelector('form');
+form.onsubmit = function() {
+  // Populate hidden form on submit
+  var about = document.querySelector('input[name=about]');
+  about.value = JSON.stringify(quill.getContents());
+  
+  console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+  
+  // No back end to actually submit to!
+  // alert('Open the console to see the submit data!')
+  return false;
+};
