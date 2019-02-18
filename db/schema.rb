96dc3bf23aaac8fb0a730bedf40463ccda2551a2
Johnny Kim <spyderkim@gmail.com>
2019-02-17 20:36:08 -0500

    fixed the assignments migration

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_02_17_232324) do
   create_table "admin", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -72,11 +73,6 @@ ActiveRecord::Schema.define(version: 2019_02_17_232324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "settings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
