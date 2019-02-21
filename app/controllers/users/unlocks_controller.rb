73faf2d72b6b5962568505f9c906789356921da2
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-02-20 23:04:54 -0500

    Changed before_filter to before_action in devise users unlock controller

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -32,7 +32,7 @@ class Users::UnlocksController < Devise::UnlocksController
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
 
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
 
